636e8f171f51e2ecacf6303dc73484b7c9b75ff9
Josueruano <110186574+Josueruano@users.noreply.github.com>
2022-09-19 17:33:41 -0500

    Update Hogar.cpp
    NEW: se cran los parámetros del constructor, se crean los constructores para cada parámetro

--- Hogar.cpp
+++ Hogar.cpp
@@ -1,4 +1,62 @@
 #include <iostream>
 #include "Hogar.h"
 
-using namespace std;
+Hogar::Hogar(){
+  
+  direccion = "";//parametros del contructor 
+  bebe = false;
+  camas = 0;
+  descripcion = "";
+}
+
+Hogar::Hogar(string direccion) : direccion(direccion){
+}
+
+Hogar::Hogar(string direccion, bool bebe) : direccion(direccion), bebe(bebe){
+}
+
+Hogar::Hogar(string direccion, bool bebe, int camas) : direccion(direccion), bebe(bebe), camas(camas){
+}
+
+Hogar::Hogar(string direccion, bool bebe, int camas, string descripcion) : direccion(direccion), bebe(bebe), camas(camas), descripcion(descripcion){
+}
+
+void Hogar::mostrarDatos() { 
+    cout << "La direccion es: " << direccion << "\n";
+    cout << "Hay bebes: " << bebes << "\n";
+    cout << "El numero de camas es: " << camas << "\n";
+    cout << "Descripcion:  " << descripcion << "\n";
+    cout << "\n";
+}
+
+string Hogar::getDireccion() const {
+    return direccion;
+}
+
+void Hogar::setDireccion(string direccion) {
+    Hogar::direccion = direccion;
+}
+
+bool Hogar::getBebe() const{
+  return bebe;
+}
+
+void Hogar::setBebe(bool bebe) {
+    Hogar::bebe = bebe;
+}
+
+int Hogar::getCamas() const{
+  return camas;
+}
+
+void Hogar::setCamas(int camas) {
+    Hogar::camas = camas;
+}
+
+string Hogar::getDescripcion() const{
+  return descripcion;
+}
+
+void Hogar::setDescripcion(int descipcion) {
+    Hogar::descipcion = descipcion;
+}
