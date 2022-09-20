a0371ff5582c4dcae99928f86158edddcf31a36e
Sebastian Dow <94850342+Sebastian-Dow@users.noreply.github.com>
2022-09-19 19:12:00 -0500

    Update Persona.cpp

---
+++ Persona.cpp
@@ -0,0 +1,12 @@
+#include "Persona.h"
+
+Persona::Persona(){
+   this->idPersona = 0;
+   this->nombre = "";
+   this->sexo = "";
+   this->puntaje = 0;
+}
+
+Persona::Persona(int long IdPersona) : idPersona(idPersona){
+}
+
